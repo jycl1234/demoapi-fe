0c4c53bcdd4ed5bb0e132e39e92588ea9bd0ff04
Jeffrey Yang <jycl1234@users.noreply.github.com>
2020-04-25 20:35:49 -0700

    style tweaks, pattern fixing

--- src/components/DisplayTable/index.tsx
+++ src/components/DisplayTable/index.tsx
@@ -1,5 +1,12 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Table, Input, Button, Row, Col } from "antd";
+import React, { useEffect, useState } from "react";
+import { Table, Input, Row, Col, Tooltip } from "antd";
+import {
+  ReloadOutlined,
+  SearchOutlined,
+  SortAscendingOutlined,
+  FileSearchOutlined,
+  CloseCircleOutlined,
+} from "@ant-design/icons";
 import "antd/dist/antd.css";
 import ErrorMessage from "./error";
 import { columns } from "./columns";
@@ -22,6 +29,7 @@ interface IApiResponse {
 
 const regexNum = /^[0-9]+$/;
 const regexAlphaNum = /^[a-zA-Z0-9 ]+$/;
+const subIconStyles = { fontSize: "1.2rem", color: "#264988" };
 
 const DisplayTable: React.FC = () => {
   const callApi = async (type: string, userInput: any): Promise<any> => {
@@ -36,14 +44,29 @@ const DisplayTable: React.FC = () => {
   const [data, setData] = useState<Items>();
   const [errMsg, setErrMsg] = useState<string>();
   const [hasError, showHasError] = useState(false);
+  const [inputVals, setInputVals] = useState({ id: "", name: "" });
+  const [showItemIdField, setShowItemIdField] = useState(false);
+  const [showItemNameField, setShowItemNameField] = useState(false);
 
-  const itemIdRef = useRef<Input>(null);
-  const itemNameRef = useRef<Input>(null);
+  const hideFields = (): void => {
+    setShowItemIdField(false);
+    setShowItemNameField(false);
+  };
+
+  const resetFields = (): void => {
+    setInputVals({ id: "", name: "" });
+  };
 
   const resetData = (): void => {
     setErrMsg("");
+    hideFields();
     showHasError(false);
     setData(undefined);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<any>): void => {
+    const { name, value } = e.target;
+    setInputVals({ ...inputVals, [name]: value });
   };
 
   const getAll = async (): Promise<any> => {
@@ -73,8 +96,9 @@ const DisplayTable: React.FC = () => {
 
   const getById = async (): Promise<any> => {
     resetData();
-    const id = itemIdRef.current?.state.value;
+    const { id } = inputVals;
     if (!id || id.search(regexNum) === -1) {
+      setErrMsg("Please use numbers only.");
       showHasError(true);
     } else {
       const { success, response }: IApiResponse = await callApi("getById", id);
@@ -89,19 +113,25 @@ const DisplayTable: React.FC = () => {
 
   const getMaxByItemName = async (): Promise<any> => {
     resetData();
-    const name = itemNameRef.current?.state.value.toUpperCase();
-    if (!name || name.search(regexAlphaNum) === -1) {
+    const { name } = inputVals;
+    if (!name) {
+      setErrMsg("Please enter an item name.");
       showHasError(true);
     } else {
-      const { success, response }: IApiResponse = await callApi(
-        "getMaxByItemName",
-        name
-      );
-      if (success) {
-        alert(`The max price for ${name} is ${response}.`);
-      } else {
-        setErrMsg(`${name} was not found.`);
+      if (name.search(regexAlphaNum) === -1) {
+        setErrMsg("Please use only alphanumeric characters.");
         showHasError(true);
+      } else {
+        const { success, response }: IApiResponse = await callApi(
+          "getMaxByItemName",
+          name.toUpperCase()
+        );
+        if (success) {
+          alert(`The max price for ${name} is ${response}.`);
+        } else {
+          setErrMsg(`${name} was not found.`);
+          showHasError(true);
+        }
       }
     }
   };
@@ -115,7 +145,99 @@ const DisplayTable: React.FC = () => {
     <div className="wrapper--outer">
       {hasError ? <ErrorMessage message={errMsg} /> : null}
       <Row>
-        <Col xs={12} className="wrapper--table">
+        <Col xs={24} className="wrapper--controls">
+          <div className="wrapper--control">
+            <Tooltip title="Get All Values">
+              <ReloadOutlined
+                className="icon--control"
+                style={{ fontSize: "1.6rem", color: "#264988" }}
+                onClick={getAll}
+              />
+            </Tooltip>
+          </div>
+          <div className="wrapper--control">
+            <Tooltip title="Get Max Prices Sorted by Item Name">
+              <SortAscendingOutlined
+                className="icon--control"
+                style={{ fontSize: "1.6rem", color: "#264988" }}
+                onClick={getMaxPrices}
+              />
+            </Tooltip>
+          </div>
+          <div className="wrapper--control">
+            <Tooltip title="Find Item by Id">
+              <SearchOutlined
+                className="icon--control"
+                style={{ fontSize: "1.6rem", color: "#264988" }}
+                onClick={() => {
+                  hideFields();
+                  setShowItemIdField(true);
+                }}
+              />
+            </Tooltip>
+          </div>
+          <div className="wrapper--control">
+            <Tooltip title="Get Max Price By Item Name">
+              <FileSearchOutlined
+                className="icon--control"
+                style={{ fontSize: "1.6rem", color: "#264988" }}
+                onClick={() => {
+                  hideFields();
+                  setShowItemNameField(true);
+                }}
+              />
+            </Tooltip>
+          </div>
+        </Col>
+      </Row>
+      <Row className="wrapper--inputs">
+        <Col xs={24}>
+          {showItemIdField ? (
+            <div className="wrapper--sub-controls">
+              <Input
+                name="id"
+                className="input--control get-by-id"
+                value={inputVals.id}
+                onChange={handleInputChange}
+                placeholder="Enter id"
+              />
+              <SearchOutlined
+                className="icon--sub"
+                style={subIconStyles}
+                onClick={getById}
+              />
+              <CloseCircleOutlined
+                className="icon--sub"
+                style={subIconStyles}
+                onClick={resetFields}
+              />
+            </div>
+          ) : null}
+          {showItemNameField ? (
+            <div className="wrapper--sub-controls">
+              <Input
+                name="name"
+                className="input--control get-by-name"
+                value={inputVals.name}
+                onChange={handleInputChange}
+                placeholder="Enter name"
+              />
+              <SearchOutlined
+                className="icon--sub"
+                style={subIconStyles}
+                onClick={getMaxByItemName}
+              />
+              <CloseCircleOutlined
+                className="icon--sub"
+                style={subIconStyles}
+                onClick={resetFields}
+              />
+            </div>
+          ) : null}
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={24} className="wrapper--table">
           <Table
             className="item--table"
             columns={columns}
@@ -128,30 +250,6 @@ const DisplayTable: React.FC = () => {
               pageSizeOptions: ["5", "10", "20", "50"],
             }}
           />
-        </Col>
-        <Col xs={12} className="wrapper--controls">
-          <div className="wrapper--control">
-            <Button className="button--control" onClick={getAll}>
-              Get All Values
-            </Button>
-          </div>
-          <div className="wrapper--control">
-            <Button className="button--control" onClick={getMaxPrices}>
-              Get Max Prices Sorted by Item Name
-            </Button>
-          </div>
-          <div className="wrapper--control">
-            <Button className="button--control" onClick={getById}>
-              Get By Id
-            </Button>
-            <Input className="input--control get-by-id" ref={itemIdRef} />
-          </div>
-          <div className="wrapper--control">
-            <Button className="button--control" onClick={getMaxByItemName}>
-              Get Max Price By Item Name
-            </Button>
-            <Input className="input--control get-by-name" ref={itemNameRef} />
-          </div>
         </Col>
       </Row>
     </div>
